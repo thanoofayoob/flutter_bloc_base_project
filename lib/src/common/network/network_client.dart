7be113d35656a5960f2f55dd8def7d5ab295f10a
Thanoof Ayoob <ayoobthanoof@gmail.com>
2023-06-26 14:44:52 +0530

    added interceptors

--- lib/src/common/network/network_client.dart
+++ lib/src/common/network/network_client.dart
@@ -28,6 +28,30 @@ class DioClient {
       },
     );
 
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (RequestOptions options, RequestInterceptorHandler handler) {
+          // Do something before request is sent.
+          // If you want to resolve the request with custom data,
+          // you can resolve a `Response` using `handler.resolve(response)`.
+          // If you want to reject the request with a error message,
+          // you can reject with a `DioException` using `handler.reject(dioError)`.
+          return handler.next(options);
+        },
+        onResponse: (Response response, ResponseInterceptorHandler handler) {
+          // Do something with response data.
+          // If you want to reject the request with a error message,
+          // you can reject a `DioException` object using `handler.reject(dioError)`.
+          return handler.next(response);
+        },
+        onError: (DioException e, ErrorInterceptorHandler handler) {
+          // Do something with response error.
+          // If you want to resolve the request with some custom data,
+          // you can resolve a `Response` object using `handler.resolve(response)`.
+          return handler.next(e);
+        },
+      ),
+    );
     _dio.interceptors.add(LogInterceptor(
       requestHeader: true,
       requestBody: true,
